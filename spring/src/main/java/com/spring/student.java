a51efd5831252e663e248f642ebb39f8b9326d51
hackone044 <souravbhunia1606@gmail.com>
2021-12-06 20:47:58 +0530

    spring practice added 06/12

---
+++ spring/src/main/java/com/spring/student.java
@@ -0,0 +1,55 @@
+package com.spring;
+
+public class student {
+
+	
+	private int studentId;
+	private String studentName;
+	private String studentAddress;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		System.out.println("settingstudent id");
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		System.out.println("setting student Name ");
+		this.studentName = studentName;
+		
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	public student(int studentId, String studentName, String studentAddress) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentAddress = studentAddress;
+	}
+	public student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "student [studentId=" + studentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
+				+ "]";
+	}
+	
+	
+	
+	
+
+	
+	
+	
+}
